7629a271f4b925d2259a02dc78304602fad6ad6d
Gabriela Dellamora Paim <gabriela@dellamora.dev>
2022-09-27 21:23:03 -0300

    Add all principal classes

--- #Desenvolvimento do projeto TG1 em aula .py
+++ #Desenvolvimento do projeto TG1 em aula .py
@@ -11,37 +11,75 @@ você está para atuar no desenvolvimento deste projeto."""
 ##Deve-se criar arquivos para armazenamento de dados
 
 
+
+
 #CLASSES
 from abc import ABC, abstractmethod
 
 class Pessoa(ABC):
-    """Classe abstrata com atributos: Nome Completo, CPF, Data de Nascimento e Estado Civil, e métodos: cadastrarDados e obterDados"""
-    def __init__(self,nomeComp,cpf,dataNasc,estadoC):
-        self._nomeComp = ""
-        self._cpf = ""
-        self._dataNasc = ""
-        self._estadoC = ""
+    """Classe abstrata | atributos: Nome Completo, CPF, Data de Nascimento e Estado Civil |  métodos: cadastrarDados e obterDados"""
+    def __init__(self, nomeComp, cpf, dataNasc, estadoC):
+        self._nomeComp = nomeComp
+        self._cpf = cpf
+        self._dataNasc = dataNasc
+        self._estadoC = estadoC
     
-    @abstractmethod
-    def cadastrarDados():
-        pass
-    
-
     @abstractmethod
     def obterDados():
         pass
 
+    
+    @abstractmethod
+    def cadastrarDados():
+        #Utilizando arquivo teste para checar funcionalidade do algoritmo
+        arquivo = open("arquivo_teste.dat", "a")
+        arquivo.writelines("TESTE \n")
+        arquivo.close()
+    
+
 
 class Medico(Pessoa):
-    """Subclasse de Pessoa, possui atributo CRM, e métodos: a internar, liberar e diagnosticar"""
-    __listaCRM = lista[]
-    def __init__(self,nomeComp,cpf,dataNasc,estadoC,crm):
+    """Subclasse de Pessoa | possui atributo CRM. |  métodos: a internar, liberar e diagnosticar"""
+    __listaCRM = listaM[]
+    def __init__(self, nomeComp, cpf, dataNasc, estadoC, crm):
         Pessoa.__init__(self,nomeComp,cpf,dataNasc,estadoC)
         
         self._crm = "" #garantir que um mesmo médico (mesmo CRM) não possa ser cadastrado duas vezes. #criar lista para testar se já existe crm !  e criar exceção
-
-    #desenvolver os métodos (acredito que obterdadops e cadastrar dados sao os getters e setters)
+    #desenvolver os métodos (acredito que obter dados e cadastrar dados sao os getters e setters)
 
 
 class Enfermeiro(Pessoa):
-    """Subclasse de Pessoa,
+    """Subclasse de Pessoa | possui atibuto COREN | métodos: cadastrarPaciente e gerarRelatorio"""
+    __listaCOREN = listaE[]
+    def __init__(self, nomeComp, cpf, dataNasc, estadoC, coren):
+        Pessoa.__init__(self, nomeComp, cpf, dataNasc, estadoC)
+
+        self._coren = "" 
+    
+    def cadastrarPaciente():
+        pass
+
+    def gerarRelatorio():
+        pass
+
+
+class Secretaria(Pessoa):
+    """Subclasse de Pessoa() | métodos: cadastrarFuncionario"""
+    def __init__(self, nomeComp, cpf, dataNasc, estadoC):
+        super().__init__(self, nomeComp, cpf, dataNasc, estadoC)
+
+    def cadastrarFuncionario():
+        pass
+
+
+class Convenio(ABC):
+    """atributos: tipo, crédito"""
+    def __init__(self, tipo, credito):
+        self.tipo = ""
+        self.credito = ""
+
+
+class Paciente(Pessoa, Convenio):
+    """Subclasse de Pessoa() | atributos: convenio"""
+    def __init__(self, nomeComp, cpf, dataNasc, estadoC):
+        super().__init__(nomeComp, cpf, dataNasc, estadoC)
